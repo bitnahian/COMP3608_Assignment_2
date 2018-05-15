d4b542102f01165ab46a1d90e47602a0131cc89b
bitnahian <nhas9102@uni.sydney.edu.au>
2018-05-15 22:14:05 +1000

    .gitignore

--- dt.py
+++ dt.py
@@ -3,10 +3,29 @@ import math
 
 # sys.argv[1] will be the csv name
 
-test1 = [['yes']]
-test2 = [['yes'],['no']]
-test3 = [['yes'],['yes'],['no'], ['no']]
-test4 = [['yes'],['yes'],['yes']]
+#test1 = [['yes']]
+#test2 = [['yes'],['no']]
+#test3 = [['yes'],['yes'],['no'], ['no']]
+#test4 = [['yes'],['yes'],['yes']]
+class Node:
+
+    def __init__(self):
+        self.attribute = None
+        self.leaves = {}
+        self.nodes = {} 
+
+    def set_attribute(self, attribute):
+        self.attribute = attribute
+
+    def add_yes_leaf(self, attr_value):
+        self.leaves[attr_value] = True
+        
+    def add_no_leaf(self, attr_value):
+        self.leaves[attr_value] = False
+
+    def add_node(self, attr_value, node):
+        self.nodes[attr_value] = node
+
 
 def entropy(fold):
     size = len(fold)
@@ -17,7 +36,3 @@ def entropy(fold):
 
     return entropy
 
-print(entropy(test1))
-print(entropy(test2))
-print(entropy(test3))
-print(entropy(test4))
