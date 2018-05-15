bf9d6b0cf9d703753f3b9e102384a6de4513303e
Shenin <sfai4579@uni.sydney.edu.au>
2018-05-15 22:13:49 +1000

    Changes made to the script

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,9 +1,14 @@
 import sys
 import math
+import csv
+
+
+
 
 def readData():
-    with open(sys.argv[1], 'r+') as training_set:
-        for line in training_set:
+    with open(sys.argv[1], newline='') as training_set:
+        reader = csv.reader(training_set, delimiter=',')
+        for row in reader:
 
 
 
